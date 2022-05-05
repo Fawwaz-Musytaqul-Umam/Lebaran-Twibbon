12ffe3fff69963bb1e45796c7eca20185bd4ba15
Fawwaz-Musytaqul-Umam <sayafawwazinifawwaz@gmail.comgit commitgit config --global user.email sayafawwazinifawwaz@gmail.com>
2022-05-05 17:36:45 +0700

    mengubah keyframe.css | menggabungkan index.js ke app.js dan mengubah type image file dari jpg menjadi png

--- js/app.js
+++ js/app.js
@@ -1,3 +1,68 @@
+const slideNextBtn = document.querySelector('.next');
+const slidePrevBtn = document.querySelector('.prev');
+const twibbonImage = document.querySelector('.contents img');
+const manImage = document.querySelector('.man');
+const twibbonContainer = document.querySelector('.container-slider');
+
+//* Twibbon Slide Animation
+let i = 1;
+slideNextBtn.addEventListener("click", function (e) {
+    if (i == 6) {
+        i = 1;
+        twibbonAnimation();
+        twibbonImage.setAttribute("src", `asset/template/Twibbon${i}.png`);
+
+    } else {
+        i++;
+        twibbonAnimation();
+        twibbonImage.setAttribute("src", `asset/template/Twibbon${i}.png`);
+    }
+});
+
+
+slidePrevBtn.addEventListener("click", function (e) {
+    if (i == 1) {
+        i = 6;
+        twibbonAnimation();
+        twibbonImage.setAttribute("src", `asset/template/Twibbon${i}.png`);
+
+    } else {
+        i--;
+        twibbonAnimation();
+        twibbonImage.setAttribute("src", `asset/template/Twibbon${i}.png`);
+    }
+});
+
+function twibbonAnimation() {
+    twibbonImage.style.animation = 'twibbon-img .2s';
+
+    setTimeout(() => {
+        twibbonImage.style.removeProperty('animation')
+    }, 200);
+}
+
+window.addEventListener('scroll', function () {
+    let scrollY = window.scrollY;
+
+    //* Row-2 man Img Animation
+    if (scrollY >= 530) {
+        manImage.style.animation = `man-img 1s`;
+        manImage.style.right = `-55vw`;
+    } else {
+        manImage.style.right = `-100vw`;
+        manImage.style.removeProperty('animation');
+    }
+
+    //* Twibbon Slider Animation
+    if (scrollY >= 467) {
+        twibbonContainer.style.transition = '.8s';
+        twibbonContainer.style.opacity = 1;
+    } else {
+        twibbonContainer.style.opacity = 0;
+    }
+})
+
+// ketika twibbon dipilih
 const preview = twibbonContainer.querySelector('.preview')
 const pilihTwibbon = twibbonContainer.querySelector('.pilih-twibbon');
 const upload = twibbonContainer.querySelector(".upload");
@@ -10,30 +75,32 @@ pilihTwibbon.addEventListener('click', function () {
 
     upload.style.display = 'block';
     reload.style.display = 'block';
-    input.style.display = 'block';
     preview.innerHTML = `<img src="${twibbonImage.src}" id="twibbon" style="width: 80vw;">`;
 
     const canvas = document.createElement("canvas");
     canvas.setAttribute("id", "canvas");
     canvas.setAttribute("hidden", "true");
-    preview.appendChild(canvas)
+    preview.appendChild(canvas);
 });
+
+upload.addEventListener("click", function () {
+    input.click();
+})
 
 document.querySelector("input").addEventListener("change", function (e) {
     const selesai = twibbonContainer.querySelectorAll(".button .selesai");
     const download = document.querySelector(".download");
-    const share = document.querySelector(".share");
 
     const user = document.createElement("img");
     user.src = URL.createObjectURL(e.target.files[0]);
 
     user.onload = () => {
-        drawCanvas(user, share);
+        drawCanvas(user);
         downloadLink(download, selesai);
     }
 })
 
-function drawCanvas(user, share) {
+function drawCanvas(user) {
     const gambarTwibbon = document.getElementById('twibbon');
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
@@ -49,13 +116,13 @@ function drawCanvas(user, share) {
     input.remove()
 
     reload.style.width = "10vw";
-    canvas.removeAttribute("hidden")
-    _share(share, canvas)
+    canvas.removeAttribute("hidden");
+    _share(canvas);
 }
 
 function downloadLink(download, selesai) {
     const link = document.createElement("a");
-    link.href = canvas.toDataURL("image/jpg");
+    link.href = canvas.toDataURL("image/png");
 
     selesai.forEach(e => {
         e.style.display = 'block';
@@ -69,8 +136,10 @@ function downloadLink(download, selesai) {
     })
 }
 
-function _share(share, image) {
+function _share(image) {
+    const share = document.querySelector(".share");
     const shareLink = document.createElement("a");
+
     shareLink.href = linkText(image);
     share.addEventListener("click", function () {
         shareLink.click();
@@ -78,8 +147,11 @@ function _share(share, image) {
 }
 
 function linkText(image) {
-    return `whatsapp://send?text=${image.toDataURL('image/jpg')}
-    Kumandang takbir bergema, kerinduaan akhirnya berujung temu, kebahagiaan singgah di setiap rumah, maka izinkan kusampaikan minal aidzin wal faidzin, mohon maaf lahir dan batin.Selamat Idul Fitri 2022
+    return `whatsapp://send?text=${encodeURIComponent(image)} Kumandang takbir bergema, kerinduaan akhirnya berujung temu, kebahagiaan singgah di setiap rumah, maka izinkan kusampaikan minal aidzin wal faidzin, mohon maaf lahir dan batin.
+
+    Selamat hari Raya Idul Fitri 1443H
+
+
     mau buat foto menyambut lebaran juga??
     klik link ini https://Fawwaz-Musytaqul-Umam.github.io/Lebaran-Twibbon`;
 }
